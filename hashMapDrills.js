ee2c33cb7df6a1f63730af3cd4e28bfba323256c
Michael Weedman <michael.weedman@gmail.com>
2020-01-26 09:49:36 -0700

    Working on 1

--- hashMapDrills.js
+++ hashMapDrills.js
@@ -2,4 +2,14 @@ const HashMap = require('./HashMap');
 
 const main = function() {
   const lotr = new HashMap();
+  lotr.MAX_LOAD_RATIO = 0.5;
+  lotr.SIZE_RATIO = 3;
+  lotr.set('Hobbit', 'Bilbo');
+  lotr.set('Hobbit', 'Frodo');
+  lotr.set('Wizard', 'Gandalf');
+  lotr.set('Human', 'Aragorn');
+
+  console.log(lotr._hashTable);
 };
+
+main();
